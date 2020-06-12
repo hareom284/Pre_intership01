0a3c8a0403f3fda5c795891c0a46fda7cd9655e1
hareom284 <zawzawwinucsl@gmail.com>
2020-06-13 00:03:44 +0630

    added ajax and ..

---
+++ Java_Script/uploadforjs/testajax/submitData.php
@@ -0,0 +1,17 @@
+<?php
+
+    
+$output = '' ;
+
+$output .='<option value="Test1">Test1</option>';
+$output .='<option value="Test2">Test2</option>';
+$output .='<option value="Test3">Test3</option>';
+
+  
+
+
+echo $output;
+
+
+
+?>
